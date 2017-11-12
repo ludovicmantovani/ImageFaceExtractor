f30d8469ff198801880515ab9975383b0b7393f3
Ludovic <ludovic.mantov@gmail.com>
2017-11-12 20:19:39 +0100

    Ajout système de log

--- ImageFaceExtractor.py
+++ ImageFaceExtractor.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import logging
+
 import OpenCVGenericDetection
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     genericClass = OpenCVGenericDetection.OpenCVGenericDetection('')
 
